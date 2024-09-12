143c55f0c7fa6f5e31b74ce80eeb5f7ddc5d5dde
leandroVR1 <yeifry.121@gmail.com>
2024-09-12 09:41:49 -0500

    ajustes fronted

--- Program.cs
+++ Program.cs
@@ -39,6 +39,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=UsuariosView}/{action=Login}/{id?}");
+    //pattern: "{controller=SedeView}/{action=Sedes}/{id?}");
 
 app.Run();
